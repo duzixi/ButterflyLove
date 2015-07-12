655caa33a13b38edfd21a1a73450ed9285225e7e
apple <duzixi@lanou3g.com>
2015-07-12 15:00:20 +0800

    Ver 1.0.1

---
+++ Assets/_Script/IKAnime.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 脚本功能：通过空对象位置控制IK动画
+/// 添加对象：UnityChan
+/// 创建时间：2015年7月8日
+/// 知识要点：
+/// 1. IK动画
+/// </summary>
+public class IKAnime : MonoBehaviour {
+
+	Animator animator;
+	public Transform rightHandPos;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	// IK动画响应方法
+	void OnAnimatorIK() {
+		// 设置右手权重
+		animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+		animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+		// 设置右手位置
+		animator.SetIKPosition(AvatarIKGoal.RightHand, 
+		                       rightHandPos.position);
+		animator.SetIKRotation(AvatarIKGoal.RightHand,
+		                       rightHandPos.rotation);
+	}
+}
---
+++ Assets/_Script/LookAtMouse.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtMouse : MonoBehaviour {
+
+	Animator animator;
+	public Transform butterFly;
+
+
+	void Start () {
+		animator = GetComponent<Animator>();
+		animator.SetLookAtWeight(1, 0.1f, 0.8f, 0.9f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit)) {
+			print (hit.point);
+            butterFly.position = hit.point;
+			animator.SetLookAtWeight(1, 0.1f, 0.8f, 0.9f);
+            animator.SetLookAtPosition(hit.point);
+		}
+        butterFly.LookAt(transform);
+	}
+}
