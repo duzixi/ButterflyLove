6ce42315fbc96076f26f20fb89dec0def896ebc2
apple <duzixi@lanou3g.com>
2015-07-12 19:17:44 +0800

    add 注释

--- Assets/_Script/LookAtMouse.cs
+++ Assets/_Script/LookAtMouse.cs
@@ -1,27 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/// <summary>
+/// 脚本功能：角色看着飞舞的蝴蝶
+/// 添加对象：角色
+/// 创建时间：2015年7月11日 duzixi.com
+/// 知识要点：
+/// 1. Animator.SetLookAtWeight()
+/// 2. Animator.SetLookAtPosition()
+/// </summary>
 public class LookAtMouse : MonoBehaviour {
 
-	Animator animator;
-	public Transform butterFly;
-
+	Animator animator;                                      // 角色动画组件
+	public Transform butterFly;                             // 飞舞的蝴蝶
 
 	void Start () {
-		animator = GetComponent<Animator>();
-		animator.SetLookAtWeight(1, 0.1f, 0.8f, 0.9f);
+		animator = GetComponent<Animator>();                // 获取角色动画组件
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
+		// 时刻监测鼠标打在角色面前透明平面的点
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 		if (Physics.Raycast(ray, out hit)) {
 			print (hit.point);
-            butterFly.position = hit.point;
-			animator.SetLookAtWeight(1, 0.1f, 0.8f, 0.9f);
-            animator.SetLookAtPosition(hit.point);
+			butterFly.position = hit.point;                 // 设置蝴蝶位置
+
+			// 设置上半身朝向目标的权重： 整体、身体、头部、眼镜
+			animator.SetLookAtWeight(1, 0.1f, 0.8f, 0.9f);  
+			animator.SetLookAtPosition(hit.point);          // 设置所看目标位置
 		}
-        butterFly.LookAt(transform);
+		butterFly.LookAt(transform);                        // 蝴蝶时刻看着角色
 	}
 }
